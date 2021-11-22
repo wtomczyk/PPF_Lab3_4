3ff3a350a3b900dd97bb9a3fff831245f178615c
wtomczyk <wojciechtomczykpl@gmail.com>
2021-11-22 15:06:45 +0100

    a2

---
+++ 02VARIABLES/01var.js
@@ -0,0 +1,32 @@
+var x = 4
+var y = 6
+var z = x+y
+
+var name = "Paweł"
+
+var person = "Karolina Kowalska", carName = "Nissan", price=20000
+
+var personA = "Karolina Kowalska",
+carNameA = "Nissan",
+priceA = 20000
+
+var carNameB
+var personX = "Janina" + " " + "Nowak"
+
+var isFamous = true
+var xx = 2 + 3 + "5"
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(name)
+console.log(person)
+console.log(carName)
+console.log(price)
+console.log(personA)
+console.log(carNameA)
+console.log(priceA)
+console.log(carNameB)
+console.log(personX)
+console.log(isFamous)
+console.log(xx)
